35a2241d904d3bf103d70079fd3c0123e2755c32
Piyush Mehta <me@piyushmehta.com>
2018-02-23 08:47:59 +0530

    Update BINARY SEARCH (dot Model) .asm

--- MicroProcessor Lab Programs/BINARY SEARCH (dot Model) .asm
+++ MicroProcessor Lab Programs/BINARY SEARCH (dot Model) .asm
@@ -1,129 +1,36 @@
-.MODEL SMALL
-
-.STACK 100H
-
-.DATA
-
-        MATR1 DW 0001H,0002H,0003H,0004H,0005H,0006H,0007H,0008H,0009H,000AH
-
-        SEARCH DW 0003H
-
-        LOWER DW 0000H
-
-        HIGHER DW 000AH
-
-        MID DW 0000H
-
-        TEMP DW 0000H
-
-    CI DW 0000H
-
-        MSG1 DB 'NUMBER FOUND','$'
-
-        MSG2 DB 'NUMBER NOT FOUND','$'
-
-.CODE
-
-MAIN PROC FAR
-
-        MOV AX,@DATA
-
-        MOV DS,AX
-
-        MOV BX,0002H
-
-        LEA DI,MATR1
-
-        LEA SI,MATR1+0014H
-
-NEXT:   MOV AX,LOWER
-
-        MOV DX,HIGHER
-
-        CMP AX,DX
-
-        JGE TERM2
-
-        MOV AX,0
-
-        MOV DX,0
-
-        MOV AX,LOWER
-
-        ADD AX,HIGHER
-
-        DIV BX
-
-        MOV SI,0000H
-
-        MOV SI,AX
-
-        ;MOV AL,SEARCH
-
-    MOV CI,AX
-
-    MUL BX
-
-    MOV CX,AX
-
-    MOV AX,CI
-
-    MOV CI,CX
-
-        CMP [SI+CI],SEARCH
-
-        JGE AGAIN
-
-        MOV HIGHER,AX
-
-        JMP NEXT
-
-AGAIN:  MOV SI,0000H
-
-        MOV SI,AX
-
-        MOV DX,AX
-
-    MOV CI,AX
-
-    MUL BX
-
-    MOV CX,AX
-
-    MOV AX,CI
-
-    MOV CI,CX
-
-    MOV AX,0
-
-        CMP [SI+CI],SEARCH
-
-        JE TERM
-
-        MOV LOWER,DX
-
-        JMP NEXT
-
-TERM:   LEA DX,MSG1
-
-        MOV AH,09H
-
-        INT 21H
-
-        MOV AH,4CH
-
-        INT 21H
-
-TERM2:  LEA DX,MSG2
-
-        MOV AH,09H
-
-        INT 21H
-
-        MOV AH,4CH
-
-        INT 21H
-
-MAIN ENDP
-
-END
+.model small
+.data
+a dw 1234h,5678h,6252h,7617h,8213h ;numbers in ascending order
+n equ 5
+key dw 1234h ;keytobesearched
+msg1 db 10,13,"Key is found$"
+msg2 db 10,13,"Key is not found$"
+.code
+Mov ax,@data
+Mov ds,ax ;Initialize thedatasegment
+Mov ax,key
+mov cx,0
+mov dx,n-1
+loop1:cmp cx,dx ja
+notfound
+mov bx,cx
+add bx,dx
+shr bx,1
+mov si,bx
+add si,si ;to point to the actual mid
+cmp ax,a[si] je
+found
+ja above
+mov dx,bx ;high=mid-1
+dec dx
+jmp loop1
+above:mov cx,bx ;low=mid+1
+inc cx
+jmp loop1
+found:lea dx,msg1
+jmp print notfound:lea
+dx,msg2 print: mov ah,09h
+int 21h
+mov ah,4ch int
+21h
+end
