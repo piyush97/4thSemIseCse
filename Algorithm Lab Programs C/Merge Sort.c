bb7f206e160b778e530f3e335c6adaacde880c56
Piyush Mehta <me@piyushmehta.com>
2018-03-19 07:19:45 +0530

    Update Merge Sort.c

--- Algorithm Lab Programs C/Merge Sort.c
+++ Algorithm Lab Programs C/Merge Sort.c
@@ -21,14 +21,14 @@ void merge(int a[],int low,int mid,int high)
         for(p=0; p<k; p++)
                 a[low++]=b[p];
 }
-void mergesort(int a[],int low,int high)
+void mergeSort(int a[],int low,int high)
 {
         int mid;
         if(low<high)
         {
                 mid=(low+high)/2;
-                mergesort(a,low,mid);
-                mergesort(a,mid+1,high);
+                mergeSort(a,low,mid);
+                mergeSort(a,mid+1,high);
                 merge(a,low,mid,high);
         }
 }
@@ -43,11 +43,11 @@ void main()
                 a[n-i]=i;
         clock_t start,end;
         start=clock();
-//printf("sorted array is \n");
-        mergesort(a,0,n-1);
+        printf("sorted array is \n");
+        mergeSort(a,0,n-1);
         end=clock();
-//for(i=0;i<n;i++)
-//printf("%d",a[i]);
+        for(i=0; i<n; i++)
+                printf("%d",a[i]);
         float time=(float)(end-start)/CLOCKS_PER_SEC;
         printf("time takenis = %f",time);
 }
