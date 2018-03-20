a53e8e783a5baa89817840c23d937c3caeb38051
Piyush Mehta <me@piyushmehta.com>
2018-03-20 08:25:22 +0530

    Update Quick Sort.c

--- Algorithm Lab Programs C/Quick Sort.c
+++ Algorithm Lab Programs C/Quick Sort.c
@@ -35,16 +35,16 @@ int main(){
         printf("How many elements are u going to enter?: ");
         scanf("%d",&count);
         int number[count];
-        for(i=0; i<count; i++)
-                number[i]=rand();
+        for(i=count; i>0; i--)
+                number[count-i]=i;
         clock_t start,end;
         start=clock();
         quicksort(count,number,0,count-1);
         end=clock();
-        /* printf("Order of Sorted elements: ");
-           for(i=0;i<count;i++)
-            printf(" %d",number[i]);*/
+        printf("Order of Sorted elements: ");
+        for(i=0; i<count; i++)
+                printf(" %d",number[i]);
         float time=(float)(end-start)/CLOCKS_PER_SEC;
-        printf("time taken is = %f",time);
+        printf("time taken is = %f ",time);
         return 0;
 }
