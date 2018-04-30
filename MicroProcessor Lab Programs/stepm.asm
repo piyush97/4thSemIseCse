4f9d79d7e586fc7f17dd690f302ea1b8918d76f7
Piyush Mehta <me@piyushmehta.com>
2018-04-30 23:57:59 +0530

    Update stepm.asm

--- MicroProcessor Lab Programs/stepm.asm
+++ MicroProcessor Lab Programs/stepm.asm
@@ -1,46 +1,42 @@
-assume cs:code,ds:data
-
+assume cs:code, ds:data
 data segment
-pa equ 20a0h
-pb equ 20a1h
-pc equ 20a2h
-cr equ 20a3h
-cw equ 80h
+         pa equ 20a0h
+         pb equ 20a1h
+         pc equ 20a2h
+         cr equ 20a3h
 data ends
 
 code segment
-delay proc
-      mov si,0fffh
-  l2: mov di,0ffffh
-  l1: dec di
-      jnz l1
-      dec si
-      jnz l2
-      ret
-delay endp
+    start:
+           mov ax,data
+           mov ds,ax
 
+           mov dx,cr
+           mov al,80h
+           out dx,al
 
-start:
-	mov ax,data
-	mov ds,ax
-	
-	mov dx,cr
-        mov al,80h
-        out dx,al
+           mov cx,10h     ;degree of rotation
+           mov al,22h     ; the quadrant
 
-        mov cx,50d
+           mov dx,pa
 
-        mov al,11h
+    rot_clock:
+               out dx,al
+               ror al,1             ;clockwise
+               call delay
+               loop rot_clock
+               mov ah,4ch
+               int 21h
 
-        mov dx,pa
- nexts: out dx,al
-       ; call delay
-        rol al,1
-        loop nexts
+    delay proc
+              mov bx,000ffh
+         l2:  mov di,02fffh
+         l1:  dec di
+              jnz l1
+              dec bx
+              jnz l2
+              ret
+          delay endp
 
-	
-	mov ah,4ch
-	int 21h
 code ends
 end start
-	
