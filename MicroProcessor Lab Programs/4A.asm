a3f5bca8e00fba21f5f2af7689d58eacfca5c17d
Piyush Mehta <me@piyushmehta.com>
2018-06-06 14:28:08 +0530

    Update and rename palin.asm to 4A.asm

--- MicroProcessor Lab Programs/4A.asm
+++ MicroProcessor Lab Programs/4A.asm
@@ -1,7 +1,7 @@
 assume cs:code,ds:data
 data segment
         str1 db 'madam'
-        n dw  n – str1
+        n    dw  n – str1 
         str2 db 5 dup(?)
         msg1 db "pallindrome$"
         msg2 db "not a palindrome$"
@@ -29,17 +29,16 @@ nextchar:mov al,[si]
         lea di,str2
         cld		; so that si and di will be incremented
         mov cx,n
-        repz cmpsb
+        rep cmpsb
         jnz unsuccess
         lea dx,msg1
         jmp disp
+        
 unsuccess:lea dx,msg2
-
 
     disp:mov ah,9h
          int 21h
-
-         mov ah,4ch
+         mov ah,07h
          int 21h
 
          code ends
